e2b657be760b83cbb5a9883239884da539e633d4
VamosHuiJia <giahuy20092004@gmail.com>
2025-11-15 09:41:12 +0700

    done

--- src/Admin/componentsAD/Topbar.js
+++ src/Admin/componentsAD/Topbar.js
@@ -1,7 +1,7 @@
 export default function Topbar(){
   return (
     <header className="ad-topbar">
-      <div>Admin Console</div>
+      <div>Nguyễn Văn A</div>
       <div className="ad-muted" style={{fontSize:14}}>admin@shop.com</div>
     </header>
   );
