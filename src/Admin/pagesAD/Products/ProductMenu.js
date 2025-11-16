a9757f3dcd68b3167896e34b61180299bedb8a1b
VamosHuiJia <giahuy20092004@gmail.com>
2025-11-16 18:23:46 +0700

    Toi uu form products

--- src/Admin/pagesAD/Products/ProductMenu.js
+++ src/Admin/pagesAD/Products/ProductMenu.js
@@ -6,9 +6,15 @@ export default function ProductMenu() {
   return (
     <div className="ad-card ad-add">
       <div className="ad-body" style={{display:'flex',justifyContent:'space-between',alignItems:'center'}}>
-        <button className="ad-btn" onClick={() => dispatch({ type: "addProductModal", payload: true })}>
+        <button className="ad-btn"
+          onClick={() => {
+            dispatch({ type: "editProductModalClose" });          
+            dispatch({ type: "addProductModal", payload: true }); 
+          }}
+        >
           + Thêm sản phẩm
         </button>
+
         {/* Sau này nhúng AddProductModal / EditProductModal nếu muốn */}
       </div>
     </div>
